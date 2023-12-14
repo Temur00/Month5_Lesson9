e2cced2d2876adf6b6d8180bc1edf2495fbfbde8
Temur <shernazarovtemurcom001@gmail.com>
2023-12-14 16:07:00 +0500

    added

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const currentTime = document.getElementById("clock");
+const date = new Date();
+const options = {
+  weekday: "long",
+  month: "long",
+  day: "numeric",
+};
+const date3 = date.toLocaleDateString("en-IN", options);
+document.getElementById("formattedDate").innerHTML += date3;
+
+var clock = new Date();
+var current_time =
+  date.getHours() + ":" + clock.getMinutes() + ":" + clock.getSeconds();
+document.getElementById("clock").innerHTML = current_time;
